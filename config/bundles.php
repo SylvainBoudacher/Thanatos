1bfdfdead040c81de27dfab6f9f0128ea2f11c25
Norudah <pierucci.romain@gmail.com>
2022-02-19 17:00:03 +0100

    Start over the whole registration and auth process

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
