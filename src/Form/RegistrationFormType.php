a4449521450f7e000baf9badf05c6ba9f23c4b24
Sulyk <sylvain.boudacher@gmail.com>
2022-02-19 18:05:15 +0100

    Update address input to inputText

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -28,9 +29,8 @@ class RegistrationFormType extends AbstractType
                 // this is actually the default format for single_text
                 'format' => 'yyyy-MM-dd',
             ])
-            ->add('address')
+            ->add('address' , TextType::class)
             ->add('email')
-
 
             ->add(
                 'plainPassword',
