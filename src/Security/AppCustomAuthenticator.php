8cf6ff57eb62550446f6921847e290ee364e6305
Sulyk <sylvain.boudacher@gmail.com>
2022-03-15 21:34:56 +0100

    Add custom login

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -51,8 +51,8 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('homepage'));
-        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        /*throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);*/
     }
 
     protected function getLoginUrl(Request $request): string
