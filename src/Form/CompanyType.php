f685eb0a02b6beca5a22eddcecc14d9b15dcf888
ZERRAI Sami <sami.zerrai@gmail.com>
2022-03-18 21:51:23 +0100

    🚧 Advancement on form company/driver

---
+++ src/Form/CompanyType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class CompanyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('siret', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex('/^[0-9]{14}$/')
+                ],
+            ])
+
+            ->add('iban', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex('/^(?:(?:IT|SM)\d{2}[A-Z]\d{22}|CY\d{2}[A-Z]\d{23}|NL\d{2}[A-Z]{4}\d{10}|LV\d{2}[A-Z]{4}\d{13}|(?:BG|BH|GB|IE)\d{2}[A-Z]{4}\d{14}|GI\d{2}[A-Z]{4}\d{15}|RO\d{2}[A-Z]{4}\d{16}|KW\d{2}[A-Z]{4}\d{22}|MT\d{2}[A-Z]{4}\d{23}|NO\d{13}|(?:DK|FI|GL|FO)\d{16}|MK\d{17}|(?:AT|EE|KZ|LU|XK)\d{18}|(?:BA|HR|LI|CH|CR)\d{19}|(?:GE|DE|LT|ME|RS)\d{20}|IL\d{21}|(?:AD|CZ|ES|MD|SA)\d{22}|PT\d{23}|(?:BE|IS)\d{24}|(?:FR|MR|MC)\d{25}|(?:AL|DO|LB|PL)\d{26}|(?:AZ|HU)\d{27}|(?:GR|MU)\d{28})$/')
+                ],
+            ])
+            ->add('rib', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex('/(0[1-9]|[1-8]\d|9[0-7])$/')
+                ],
+            ])
+            ->add('bic', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex('/^[a-z]{6}[0-9a-z]{2}([0-9a-z]{3})?\z/i')
+                ],
+            ])
+            ->add('address', AddressType::class)
+
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'save w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600 mt-4'],
+            ])
+
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les termes.',
+                    ]),
+                ],
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
