e242704241aa12ae44fc953771907fc403d3d615
Coraline <cesedji@myges.fr>
2022-03-24 18:58:43 +0100

    :bug: fix route error + fix order error

--- src/Repository/OrderRepository.php
+++ src/Repository/OrderRepository.php
@@ -50,7 +50,7 @@ class OrderRepository extends ServiceEntityRepository
 
         $query = $qb->getQuery();
 
-        return $query->getSingleResult();
+        return $query->execute();
     }
 
 
