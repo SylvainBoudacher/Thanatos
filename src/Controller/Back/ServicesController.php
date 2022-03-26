947b6cd423499bff64a7199613bffb83e517cdb0
Norudah <pierucci.romain@gmail.com>
2022-03-26 20:55:00 +0100

    Company can suscribes to a theme + add toasts component with animations

--- src/Controller/Back/ServicesController.php
+++ src/Controller/Back/ServicesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Back;
 
 use App\Entity\Burial;
+use App\Entity\CompanyTheme;
 use App\Entity\Extra;
 use App\Entity\Material;
 use App\Entity\Media;
@@ -16,11 +17,13 @@ use App\Form\ModelType;
 use App\Form\PaintingType;
 use App\Repository\BurialRepository;
 use App\Repository\CompanyRepository;
+use App\Repository\CompanyThemeRepository;
 use App\Repository\ExtraRepository;
 use App\Repository\MaterialRepository;
 use App\Repository\ModelMediaRepository;
 use App\Repository\ModelRepository;
 use App\Repository\PaintingRepository;
+use App\Repository\ThemeRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -424,6 +427,62 @@ class ServicesController extends AbstractController
 
         return $this->redirectToRoute("view_extras");
     }
+
+    #[Route('/company-themes', name: 'view_company_themes')]
+    public function view_company_themes(ThemeRepository $themeRep, CompanyThemeRepository $companyThemeRep, UserRepository $userRep ) : Response {
+        $user = $userRep->find($this->getUser());
+        $company = $user->getCompany();
+
+        $themes = $themeRep->findAll();
+        $themesSuscribedByCompany = $themeRep->getAllByCompany($company);
+
+        $detailedThemes = [];
+
+        foreach ($themes as $theme) {
+            $detailedThemes[] = [
+                "theme" => $theme,
+                "isSuscribed" => in_array($theme, $themesSuscribedByCompany, true),
+            ];
+        }
+
+        return $this->render("back/company/services/company_themes/index.html.twig", [
+            "detailedThemes" => $detailedThemes
+        ]);
+    }
+
+    #[Route('/company-themes/switch/{id}', name: 'switch_company_themes')]
+    public function switch_company_themes(EntityManagerInterface $em,  ThemeRepository $themeRep, int $id, CompanyRepository $companyRep, UserRepository $userRep, CompanyThemeRepository $companyThemeRep) : Response {
+        $user = $userRep->find($this->getUser());
+        $company = $user->getCompany();
+        $theme = $themeRep->find($id);
+
+        // TODO : Meilleur vérification plus tard (genre theme désactivé par thanatos)
+        if (!empty($company)) {
+
+            $companyTheme = $companyThemeRep->getOneByCompanyAndTheme($company, $theme);
+
+            if ($companyTheme) {
+                $em->remove($companyTheme);
+                $em->flush();
+                $this->addFlash("success", "Vous vous êtes désinscrit du thème ".$theme->getName().".");
+                return $this->redirectToRoute("view_company_themes");
+            }
+
+            $companyTheme = new CompanyTheme();
+            $companyTheme->setCompany($company);
+            $companyTheme->setTheme($theme);
+
+            $em->persist($companyTheme);
+            $em->flush();
+            $this->addFlash("success", "Vous vous êtes inscrit au thème ".$theme->getName().".");
+            return $this->redirectToRoute("view_company_themes");
+        }
+
+        $this->addFlash("error", "Une erreur est survenue. Veuillez réessayez ultérieurement");
+        return $this->redirectToRoute("view_company_themes");
+    }
+
+
 
 
 }
