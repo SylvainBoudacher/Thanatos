6237e109ca8691585393b60ce25f648530270f77
Coraline <cesedji@myges.fr>
2022-07-08 08:29:01 +0200

    Install pagination knb with composer

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
